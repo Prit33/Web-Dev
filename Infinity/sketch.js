6b369372818c65ed1744dc9264acd9e1bb4f031d
Prit <74133659+Prit33@users.noreply.github.com>
2021-05-06 18:13:27 +0530

    Add files via upload

---
+++ Infinity/sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(1366, 644, WEBGL);
+  angleMode(DEGREES)
+}
+
+function draw() {
+  background(30);
+  noFill()
+  rotateX(60)
+  // rotateY(90)
+  // let r=random(100,500);
+  stroke(255)
+  for (var i = 0; i < 80; i++) {
+
+
+    var r = map(sin(frameCount / 2), -1, 1, 500, 100)
+    var g = map(1, 0, 255, 100, 200)
+    var b = map(cos(frameCount), -1, 1, 300, 100)
+
+    stroke(r, g, b)
+
+    beginShape()
+    for (var j = 0; j <= 360; j += 5) {
+
+      var rad = i * 2;
+      var x = rad * cos(j) * 1.5
+      var y = rad * sin(j * 2)
+      var z = cos(cos(frameCount * 40 + i * 10) * 100) * 30
+
+      vertex(x, y, z)
+    }
+    endShape(CLOSE)
+
+
+  }
+
+}
